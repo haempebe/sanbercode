84373090c6b8955e60159642771bb658030c3506
haempebe <hamidzanpranajanalabhamkerti@gmail.com>
2023-10-09 16:47:08 +0700

    commit PHP-1: Tugas 7 String & Array

--- php-1/array.php
+++ php-1/array.php
@@ -19,8 +19,8 @@
             Kids : "Mike", "Dustin", "Will", "Lucas", "Max", "Eleven" 
             Adults: "Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"
         */
-    $kids; // Lengkapi di sini
-    $adults;
+    $kids = ["Mike", "Dustin", "Will", "Lucas", "Max", "Eleven"]; // Lengkapi di sini
+    $adults = ["Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"];
     echo "<h3> Soal 2</h3>";
     /* 
             SOAL NO 2
@@ -28,19 +28,28 @@
         */
     echo "Cast Stranger Things: ";
     echo "<br>";
-    echo "Total Kids: "; // Berapa panjang array kids
+    echo "Total Kids: " . count($kids); // Berapa panjang array kids
     echo "<br>";
     echo "<ol>";
     echo "<li> $kids[0] </li>";
     // Lanjutkan
+    echo "<li> $kids[1] </li>";
+    echo "<li> $kids[2] </li>";
+    echo "<li> $kids[3] </li>";
+    echo "<li> $kids[4] </li>";
+    echo "<li> $kids[5] </li>";
 
     echo "</ol>";
 
-    echo "Total Adults: "; // Berapa panjang array adults
+    echo "Total Adults: " . count($adults); // Berapa panjang array adults
     echo "<br>";
     echo "<ol>";
     echo "<li> $adults[0] </li>";
     // Lanjutkan
+    echo "<li> $adults[1] </li>";
+    echo "<li> $adults[2] </li>";
+    echo "<li> $adults[3] </li>";
+    echo "<li> $adults[4] </li>";
 
     echo "</ol>";
 
@@ -107,6 +116,35 @@
                 )
             
         */
+    $data = [
+        [
+            "Name" => "abduh",
+            "Age" => "12",
+            "Aliases" => "Will the Wise",
+            "Status" => "Alive"
+        ],
+        [
+            "Name" => "Mike Wheeler",
+            "Age" => "12",
+            "Aliases" => " Dugeon Master",
+            "Status" => "Alive"
+        ],
+        [
+            "Name" => "Jim Hooper",
+            "Age" => "43",
+            "Aliases" => "Chief Hopper",
+            "Status" => "Deceased"
+        ],
+        [
+            "Name" => "Eleven",
+            "Age" => "12",
+            "Aliases" => "El",
+            "Status" => "Alive"
+        ]
+    ];
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
     ?>
 </body>
 
